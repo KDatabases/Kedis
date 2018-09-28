585bb39892630c41a2ff13e964ef0272db9fa905
Sxtanna <ranald.taylor@gmail.com>
2018-09-28 15:40:29 -0400

    Renamed variable config to conf

--- src/main/kotlin/com/sxtanna/database/Kedis.kt
+++ src/main/kotlin/com/sxtanna/database/Kedis.kt
@@ -10,7 +10,7 @@ import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPoolConfig
 import java.io.File
 
-class Kedis(override val config : KedisConfig) : Database<Jedis, KedisConfig, KedisTask>() {
+class Kedis(override val conf : KedisConfig) : Database<Jedis, KedisConfig, KedisTask>() {
 
 	override val name : String = "Kedis"
 	lateinit var pool : JedisPool
@@ -19,18 +19,18 @@ class Kedis(override val config : KedisConfig) : Database<Jedis, KedisConfig, Ke
 
 	override fun load() {
 		val jedisConfig = JedisPoolConfig().apply {
-			maxTotal = config.pool.maxSize
-			maxIdle = config.pool.idleSize
+			maxTotal = conf.pool.maxSize
+			maxIdle = conf.pool.idleSize
 			testOnBorrow = true
 		}
 
-		pool = JedisPool(jedisConfig, config.server.address, config.server.port, config.pool.timeout, config.user.auth)
+		pool = JedisPool(jedisConfig, conf.server.address, conf.server.port, conf.pool.timeout, conf.user.auth)
 	}
 
 	override fun poison() = pool.destroy()
 
 
-	override fun poolResource() : Jedis? = pool.resource?.apply { select(config.user.defaultDB) }
+	override fun poolResource() : Jedis? = pool.resource?.apply { select(conf.user.defaultDB) }
 
 	override fun createTask(resource : Jedis) : KedisTask = KedisTask(resource)
 
